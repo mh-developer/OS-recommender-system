8de974820b192fc0dd16e5daafce08f3e68b6b98
Mitja <mh7860@student.uni-lj.si>
2020-01-04 13:17:54 +0100

    Prepared properly reading UserItemData

--- code/UserItemData.py
+++ code/UserItemData.py
@@ -1,34 +1,50 @@
 import pandas as pd
-
+import datetime
+datetime.datetime.strptime
 
 class UserItemData:
-    def __init__(self, path, from_date=None, end_date=None, min_ratings=-1):
+    def __init__(self, path, start_date=None, end_date=None, min_ratings=0):
         self.path = path
-        self.from_date = from_date
+        self.from_date = start_date
         self.to_date = end_date
         self.min_ratings = min_ratings
         self.df = self.read_data()
+        self.movies = self.read_data()
 
     def read_data(self):
-        if self.from_date is not None and self.to_date is not None:
-            data = pd.read_csv(self.path, sep="\t", encoding="ISO-8859-1")
-            mask = (pd.to_datetime("{}.{}.{}".format(data['date_day'], data['date_month'], data['date_year'])) > pd.to_datetime(self.from_date)) & \
-                   (pd.to_datetime("{}.{}.{}".format(data['date_day'], data['date_month'], data['date_year'])) <= pd.to_datetime(self.to_date))
-
-            return data.loc[mask]
-
-        elif self.from_date is not None:
-            return pd.read_csv(self.path, sep="\t", encoding="ISO-8859-1")
-
-        elif self.to_date is not None:
-            return pd.read_csv(self.path, sep="\t", encoding="ISO-8859-1")
-
         data = pd.read_csv(self.path, sep="\t", encoding="ISO-8859-1")
         data = data.set_index("userID", drop=False)
+
+        if self.from_date is not None or self.to_date is not None:
+            data["full_date"] = data.apply(lambda x: datetime.date(int(x['date_year']), int(x['date_month']), int(x['date_day'])), axis=1)
+            data["full_date"] = pd.to_datetime(data['full_date'])
+
+            if self.from_date is not None and self.to_date is not None:
+                day, month, year = self.from_date.split(".")
+                date_from = pd.Timestamp(datetime.date(int(year), int(month), int(day)))
+
+                day, month, year = self.to_date.split(".")
+                date_to = pd.Timestamp(datetime.date(int(year), int(month), int(day)))
+                data = data[
+                    (data['full_date'] >= date_from) &
+                    (data['full_date'] < date_to)
+                    ]
+            elif self.from_date is not None:
+                day, month, year = self.from_date.split(".")
+                date_from = pd.Timestamp(datetime.date(int(year), int(month), int(day)))
+                data = data[data['full_date'] >= date_from]
+            elif self.to_date is not None:
+                day, month, year = self.to_date.split(".")
+                date_to = pd.Timestamp(datetime.date(int(year), int(month), int(day)))
+                data = data[data['full_date'] < date_to]
+
+        if self.min_ratings > 0:
+            grouped = data.groupby("movieID")
+            data = grouped.filter(lambda x: x["movieID"].count() > self.min_ratings)
+
         return data
 
     def nratings(self):
-        print()
         return self.df.shape[0]
 
     def get_user(self, user_id):
--- code/branje_ocen.py
+++ code/branje_ocen.py
@@ -1,12 +1,7 @@
 from code.UserItemData import UserItemData
-import pandas as pd
 
 uim = UserItemData('../data/user_ratedmovies.dat')
 print(uim.nratings())
-# print(uim.head())
 
-df = UserItemData('../data/user_ratedmovies.dat').get_df()
-print(df.loc[df["movieID"] == 75, "rating"])
-
-# uim = UserItemData('../data/user_ratedmovies.dat', from_date='12.1.2007', end_date='16.2.2008', min_ratings=100)
-# print(uim.nratings())
+uim = UserItemData('../data/user_ratedmovies.dat', start_date='12.1.2007', end_date='16.2.2008', min_ratings=100)
+print(uim.nratings())
